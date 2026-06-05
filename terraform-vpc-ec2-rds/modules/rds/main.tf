98e23b8c86173c30a5495c189bae6caab5dd4129
Ketan Kakde <ketankakde@outlook.com>
2026-06-05 20:04:17 +0530

    Create main.tf

---
+++ terraform-vpc-ec2-rds/modules/rds/main.tf
@@ -0,0 +1,45 @@
+resource "aws_db_subnet_group" "rds_subnet" {
+  subnet_ids = [var.private_subnet_id]
+
+  tags = {
+    Name = "rds-subnet-group"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "mysql" {
+  identifier = "mysql-db"
+
+  engine         = "mysql"
+  engine_version = "8.0"
+
+  instance_class = "db.t3.micro"
+
+  allocated_storage = 20
+
+  db_name  = "appdb"
+  username = "admin"
+  password = "Admin@12345"
+
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  skip_final_snapshot = true
+}
