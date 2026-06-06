b0a6e3b7be54d9af8615503cf57edb30a5172c49
Ketan Kakde <ketankakde@outlook.com>
2026-06-07 00:18:09 +0530

    Change RDS username from 'localhost' to 'root'

--- terraform-vpc-ec2-rds/modules/rds/main.tf
+++ terraform-vpc-ec2-rds/modules/rds/main.tf
@@ -30,13 +30,13 @@ resource "aws_db_instance" "mysql" {
   engine         = "mysql"
   engine_version = "8.0"
 
-  instance_class = "db.t3.micro"
+  instance_class = "db.t4g.micro"
 
   allocated_storage = 20
 
-  db_name  = "appdb"
-  username = "admin"
-  password = "Admin@12345"
+  db_name  = "student_registration"
+  username = "root"
+  password = "password123"
 
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
